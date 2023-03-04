17d6f27707bd7245009310d866729a0a041b8444
Juniooor <juniormodern@hotmail.com>
2023-03-04 15:17:44 -0300

    criando end-points

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return{"message": "hello world"}
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id" : item_id}
+
+
