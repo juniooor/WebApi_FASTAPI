9848dbe978cfe8cba4b572859a5c8e442ae7cb32
Juniooor <juniormodern@hotmail.com>
2023-03-05 13:54:10 -0300

    criando crud

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from pydantic import BaseModel
+from typing import Optional
 from fastapi import FastAPI
+
+class Item(BaseModel):
+    name:str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
 app = FastAPI()
 
@@ -6,4 +14,6 @@ app = FastAPI()
 async def read_item(item_id):
     return {"item_id" : item_id}
 
-
+@app.post("/items/")
+async def post_item(item:Item):
+    return item
