39a0ce1ba42155b844b777495c9ea1244603c39f
Juniooor <juniormodern@hotmail.com>
2023-03-06 18:28:50 -0300

    end

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from pydantic import BaseModel
 from typing import Optional
+
+from pydantic import BaseModel
+
 from fastapi import FastAPI
+
 
 class Item(BaseModel):
     name:str
@@ -10,10 +13,24 @@ class Item(BaseModel):
 
 app = FastAPI()
 
+@app.get("/")
+async def root():
+    return {"message": "hello user"}
+
+
 @app.get("/items/{item_id}")
 async def read_item(item_id):
     return {"item_id" : item_id}
 
+
 @app.post("/items/")
-async def post_item(item:Item):
-    return item
+async def create_item(item:Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax":price_with_tax})
+    return item_dict
+
+# @app.put("/items/{item_id}")
+# async def create_item(item: int , item: Item):
+#     return {"item_id": item_id, **item.dict()}
